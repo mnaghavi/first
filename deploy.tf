6cb3153ef6bed3ac15c42dd86435bb4b80c3ba83
mohammad naghavi <naghavimohammad@gmail.com>
2019-09-11 11:14:31 +0430

    Add files via upload

---
+++ deploy.tf
@@ -0,0 +1,39 @@
+variable "county" {
+default = 2 
+}
+#variable "ipy" {
+#default = {
+#ip1 = "192.168.1.11"
+#ip2 = "192.168.1.12"
+#}
+#}
+resource "openstack_compute_instance_v2" "web" {
+count = "${var.county}"
+name = "${format("web-%02d", count.index+1)}"
+image_name = "CentOs 7.6 (XaaS) D5"
+availability_zone = "nova"
+region = "${var.region}"
+flavor_id = "c2"
+#key_pair = "${var.openstack_keypair}"
+security_groups = ["default"]
+network {
+name = "${var.tenant_network}"  
+}
+user_data = "${file("bootstrapweb.sh")}" 
+}
+resource "openstack_networking_floatingip_v2" "myip1" {
+  pool = "provider"
+}
+resource "openstack_networking_floatingip_v2" "myip0" {
+  pool = "provider"
+}
+resource "openstack_compute_floatingip_associate_v2" "myip" {
+  #count = "${var.county}"
+  floating_ip = "${openstack_networking_floatingip_v2.myip0.address}"
+  instance_id = "${openstack_compute_instance_v2.web[0].id}"
+#  fixed_ip    = "${openstack_compute_instance_v2.web[count.index].network.fixed_ip_v4}"
+}
+resource "openstack_compute_floatingip_associate_v2" "myip2" {
+  floating_ip = "${openstack_networking_floatingip_v2.myip1.address}"
+  instance_id = "${openstack_compute_instance_v2.web[1].id}"
+}
---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "openstack_user_name" {
+description = "The username for the Tenant."
+default  = "terraform" 
+} 
+variable "openstack_tenant_name" {
+description = "The name of the Tenant."
+default  = "naghavi" 
+} 
+variable "openstack_password" {
+description = "The password for the Tenant."
+default  = "123456_a" 
+} 
+variable "openstack_auth_url" {
+description = "The endpoint url to connect to OpenStack."
+default  = "http://vip.xaas.ir:35357/v3" 
+} 
+variable "openstack_keypair" {
+description = "The keypair to be used."
+default  = "naghavi" 
+}
+variable "tenant_network" {
+description = "The network to be used."
+default  = "naghavi" 
+}
+variable "region" {
+description = "The region to be used."
+default  = "Tehran" 
+}
